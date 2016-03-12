99de3ce0239a0e40aa4a2141b1aa4614b8895a4e
Art Burtch <artburtch@huskers.unl.edu>
2016-03-12 03:28:54 +0000

    Done

--- lib/most_frequent_integer.rb
+++ lib/most_frequent_integer.rb
@@ -3,5 +3,31 @@
 # exists, return nil.
 
 def find_most_frequent_integer(arr)
-  # put your code here
+    unique = arr.uniq
+    freq_ary = []
+    unique.each do |elem1|
+        count1 = 0
+        arr.each do |elem2|
+            if elem1 == elem2
+                count1 = count1.next
+            end
+        end
+        freq_ary.push(count1)
+    end
+    hi_freq = 0
+    freq_ary.each do |elem|
+        if elem > hi_freq
+            hi_freq = elem
+        end
+    end
+    count2 = 0
+    freq_ary.each do |elem|
+        if elem == hi_freq
+            count2 = count2.next
+        end
+    end
+    if count2 > 1
+        return nil
+    end
+    return unique[freq_ary.index(hi_freq)]
 end
